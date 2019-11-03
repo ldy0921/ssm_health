eb9d7e7451b799e6ef93d46a4f9935270fe2b3cb
刘小亮 <1078813134@qq.com>
2019-11-03 19:07:39 +0800

    调试health_backend模块

---
+++ health_parent/health_backend/src/main/java/com/jxufe/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.jxufe.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/hello.do")
+    public Map sayHello(String username, String pwd) {
+
+        System.out.println(username);
+        System.out.println(pwd);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("success", "访问成功");
+
+        return map;
+    }
+
+}
