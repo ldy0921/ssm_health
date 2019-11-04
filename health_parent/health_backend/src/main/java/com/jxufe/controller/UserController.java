cf4c56ed7c501f50e7384b54c99f56695bee5c58
刘小亮 <1078813134@qq.com>
2019-11-04 10:30:37 +0800

    删除health_backend的调式代码

--- health_parent/health_backend/src/main/java/com/jxufe/controller/UserController.java
+++ health_parent/health_backend/src/main/java/com/jxufe/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.jxufe.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
