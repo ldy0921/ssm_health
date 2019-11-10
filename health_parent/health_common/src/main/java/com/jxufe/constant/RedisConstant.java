f6342a982ed28c70893025cc8f888e54dfe26efa
刘小亮 <1078813134@qq.com>
2019-11-10 20:44:19 +0800

    图片上传 定时清除垃圾图片

--- health_parent/health_common/src/main/java/com/jxufe/constant/RedisConstant.java
+++ health_parent/health_common/src/main/java/com/jxufe/constant/RedisConstant.java
@@ -5,4 +5,7 @@ public class RedisConstant {
     public static final String SETMEAL_PIC_RESOURCES = "setmealPicResources";
     //套餐图片保存在数据库中的图片名称
     public static final String SETMEAL_PIC_DB_RESOURCES = "setmealPicDbResources";
+
+    public static final String ORDERDATE = "orderDate";
+
 }
